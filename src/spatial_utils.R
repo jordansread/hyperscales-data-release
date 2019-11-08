6a659bd234093e1dc5d9c5d7bb4348635ce216b1
Jordan S Read <jread@usgs.gov>
2019-11-07 19:48:48 -0600

    better plots, better spatial text, updated metadata

--- src/spatial_utils.R
+++ src/spatial_utils.R
@@ -51,8 +51,7 @@ subset_lake_sf <- function(lakes_sf_fl, site_ids){
   readRDS(lakes_sf_fl) %>% filter(site_id %in% !!site_ids) %>% sf::st_zm()
 }
 
-
-plot_grouped_preview <- function(fileout, spatial_groups, county_bounds, site_ids_grouped, lakes_sf_fl){
+plot_groups <- function(fileout, spatial_groups, county_bounds, lakes_sf_fl){
   png(filename = fileout, width = 8, height = 5.5, units = 'in', res = 500)
   par(omi = c(0,0,0,0), mai = c(0,0,0,0), xaxs = 'i', yaxs = 'i')
   
@@ -60,14 +59,12 @@ plot_grouped_preview <- function(fileout, spatial_groups, county_bounds, site_id
   cols <- c('#a6cee3','#b2df8a','#1f78b4','#33a02c','#fb9a99','#e31a1c','#fdbf6f','#ff7f00','#cab2d6','#6a3d9a','#b15928',
             '#e41a1c','#377eb8','#984ea3','#a65628','#f781bf','#007f7f','#ff00ff')
   
-  #set.seed(41)
-  col_vector <- rep(cols, ceiling(n/length(cols)))[1:n]#sample(cols, size = n, replace = TRUE) 
+  col_vector <- rep(cols, ceiling(n/length(cols)))[1:n]
   g_styles <- data.frame(group_id = unique(spatial_groups$group_id), col = col_vector, stringsAsFactors = FALSE)
   
   spatial_groups <- left_join(spatial_groups, g_styles, by = 'group_id')
   
   plot(st_geometry(spatial_groups), col = paste0(spatial_groups$col, '4D'), border = 'grey70', lwd = 0.1, reset = FALSE)
-
   all_lakes_simple <- readRDS(lakes_sf_fl) %>% st_transform(crs = "+init=epsg:2811") %>% sf::st_simplify(dTolerance = 40) %>% 
     st_transform(crs = "+init=epsg:4326")
   
@@ -81,11 +78,19 @@ plot_grouped_preview <- function(fileout, spatial_groups, county_bounds, site_id
   county_bounds %>% group_by(state) %>% summarise() %>% st_geometry() %>% 
     plot(col = NA, border = 'grey40', lwd = 0.5, add = TRUE)
   
+  invisible(list(sf = all_lakes_simple, style = g_styles))
+}
+
+plot_grouped_lakes_preview <- function(fileout, spatial_groups, county_bounds, site_ids_grouped, lakes_sf_fl){
+  out <- plot_groups(fileout, spatial_groups, county_bounds, lakes_sf_fl)
+  all_lakes_simple <- out$sf
+  g_styles <- out$style
+  
+
   modeled_lakes_sf <- inner_join(all_lakes_simple, site_ids_grouped, by = 'site_id') %>% 
     left_join(g_styles, by = 'group_id')
   
   plot(st_geometry(modeled_lakes_sf), col = 'dodgerblue', border = 'dodgerblue', lwd = 0.2, add = TRUE)
-  #plot(st_centroid(st_geometry(modeled_lakes_sf)), col = modeled_lakes_sf$col, lwd = 0.2, add = TRUE, cex = 0.25)
   
   for (j in 1:nrow(spatial_groups)){
     bbox <- st_bbox(spatial_groups[j,])
@@ -97,4 +102,36 @@ plot_grouped_preview <- function(fileout, spatial_groups, county_bounds, site_id
   
 }
 
+create_ldas_grid <- function(x0 = -124.9375, y0 = 25.0625, x_num = 464, y_num = 224, cell_res = 0.125){
+  ldas_crs <- "+init=epsg:4326"
+  
+  ldas_grid_sfc <- sf::st_make_grid(cellsize = cell_res, n = c(x_num, y_num),
+                                    offset = c(x0-cell_res/2, y0-cell_res/2), crs = ldas_crs)
+  # cells count left to right, then next row, then left to right
+  x_cells <- rep(0:(x_num-1), y_num)
+  y_cells <- c(sapply(0:(y_num-1), function(x) rep(x, x_num)))
+  
+  ldas_grid <- st_sf(data.frame(x = x_cells, y = y_cells), ldas_grid_sfc)
+  return(ldas_grid)
+}
 
+
+plot_grouped_cells_preview <- function(fileout, spatial_groups, county_bounds, site_ids_grouped, lakes_sf_fl, meteo_fl){
+  
+  meteos <- readRDS(meteo_fl) %>% filter(site_id %in% site_ids_grouped$site_id) %>% pull(meteo_fl) %>% unique()
+  ldas_grid <- create_ldas_grid() %>% mutate(meteo_fl = sprintf('NLDAS_time[0.351500]_x[%s]_y[%s].csv', x, y)) %>% 
+    filter(meteo_fl %in% meteos)
+  
+  
+  plot_groups(fileout, spatial_groups, county_bounds, lakes_sf_fl)
+  
+  plot(st_geometry(ldas_grid), col = '#ff00ff33', border = '#ff00ffD3', lwd = 0.2, add = TRUE)
+  
+  for (j in 1:nrow(spatial_groups)){
+    bbox <- st_bbox(spatial_groups[j,])
+    
+    text(bbox[1], bbox[2]+0.1, str_extract(spatial_groups[j,]$group_id, '[0-9]{2}'), pos = 4, cex = 0.8, offset = 0.1)
+  }
+  
+  dev.off()
+}
