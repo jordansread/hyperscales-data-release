62445f75f6860dcedf3e65dea4247d550f3a8fad
Jordan S Read <jread@usgs.gov>
2020-01-29 07:44:29 -0600

    updates now that we are including `pball`

--- src/file_utils.R
+++ src/file_utils.R
@@ -1,12 +1,13 @@
 
-extract_model_ids <- function(pb0_job_list, results_dir, dummy){
+extract_model_ids <- function(job_list_rds, results_dir, dummy){
   warning('this is a temporary function and should be replaced with an inventory of **modeled** lakes')
   files_here <- dir(results_dir)
-  job_list <- readRDS(pb0_job_list)
+  job_list <- readRDS(job_list_rds)
   # these are export files...
   job_files <- unlist(sapply(1:length(job_list), function(x) c(basename(job_list[[x]]$export_file))) %>% c())
   
-  model_ids <- files_here[files_here %in% job_files] %>% stringr::str_remove('pb0_') %>% stringr::str_remove("_temperatures.feather")
+  tibble(files = files_here[files_here %in% job_files]) %>% 
+    extract(files, c('type','site_id'), "(pb0|transfer|pball)_(.*)_temperatures.feather") %>% pull(site_id)
   
 }
 
@@ -29,9 +30,12 @@ bundle_nml_files <- function(json_filename, lake_ids, nml_ind){
   
   prep_proj_dir <- paste(str_split(nml_ind, '/')[[1]][1:2], collapse = '/')
   nml_files <- file.path(prep_proj_dir, names(yaml.load_file(nml_ind)))
+  file_bases <- tibble(file = basename(nml_files)) %>% 
+    mutate(filebase = str_remove(file, 'pball_|transfer_')) %>% pull(filebase)
   out_list <- vector("list", length = length(lake_ids)) %>% setNames(lake_ids)
+  
   for (id in names(out_list)){
-    this_nml_file <- nml_files[basename(nml_files) == paste0(id, '.nml')]
+    this_nml_file <- nml_files[file_bases == paste0(id, '.nml')]
     if (!file.exists(this_nml_file)){
       stop(this_nml_file, " doesn't exist")
     }
@@ -100,70 +104,97 @@ zip_meteo_groups <- function(outfile, grouped_meteo_fls){
   }
   scipiper::sc_indicate(outfile, data_file = data_files)
 }
+
+build_predict_df <- function(site_id_list, model_dir, dummy){
   
-zip_prediction_groups <- function(outfile, model_dir, model_ids, site_groups){
+  selected_sites <- purrr::map(names(site_id_list), function(x){
+    tibble(site_id = site_id_list[[x]], prefix = x)
+  }) %>% purrr::reduce(bind_rows)
   
-  model_feathers <- data.frame(site_id = model_ids, stringsAsFactors = FALSE) %>% 
-    mutate(model_file = sprintf('pb0_%s_temperatures.feather', site_id)) %>% 
-    inner_join(site_groups, by = 'site_id') %>% select(-site_id)
+  tibble(source_file = dir(model_dir)) %>% 
+    filter(str_detect(source_file, '_temperatures.feather$')) %>% 
+    extract(source_file, c('prefix','site_id','suffix'), "(pb0|pball|pgdl)_(.*)_(temperatures.feather)", remove = FALSE) %>% 
+    inner_join(selected_sites) %>% select(-suffix, -prefix) %>% 
+    mutate(out_file = paste0(tools::file_path_sans_ext(source_file), '.csv'), 
+           source_filepath = file.path(model_dir, source_file)) %>% select(-source_file)
+    
+}
+  
+zip_prediction_groups <- function(outfile, predictions_df, site_groups){
+  
+  model_feathers <- inner_join(predictions_df, site_groups, by = 'site_id') %>% 
+    select(-site_id)
   
   
   cd <- getwd()
   on.exit(setwd(cd))
   
-  groups <- unique(model_feathers$group_id)
+  groups <- rev(sort(unique(model_feathers$group_id)))
   data_files <- c()
   for (group in groups){
     zipfile <- paste0('tmp/predictions_', group, '.zip')
-    these_files <- model_feathers %>% filter(group_id == !!group) %>% pull(model_file)
+    these_files <- model_feathers %>% filter(group_id == !!group)
     
     zippath <- file.path(getwd(), zipfile)
     
-    these_files <- purrr::map(these_files, function(x){
-      fileout <- file.path(tempdir(), paste0(tools::file_path_sans_ext(x), '.csv'))
-      feather::read_feather(file.path(model_dir, x)) %>%
+    for (i in 1:nrow(these_files)){
+      fileout <- file.path(tempdir(), these_files$out_file[i])
+      feather::read_feather(these_files$source_filepath[i]) %>%
         select(-ice, date = DateTime) %>%
         mutate(date = as.Date(lubridate::ceiling_date(date, 'days'))) %>%
         write_csv(path = fileout)
       fileout
-    }) %>% purrr::reduce(c)
+    }
     
     setwd(tempdir())
-    zip(zippath, files = basename(these_files))
+    
+    zip(zippath, files = these_files$out_file)
+    unlink(these_files$out_file)
     setwd(cd)
     data_files <- c(data_files, zipfile)
   }
   scipiper::sc_indicate(outfile, data_file = data_files)
 }
 
-zip_ice_flags_groups <- function(outfile, model_dir, model_ids, site_groups){
-  model_feathers <- data.frame(site_id = model_ids, stringsAsFactors = FALSE) %>% 
-    mutate(model_file = sprintf('pb0_%s_temperatures.feather', site_id)) %>% 
-    inner_join(site_groups, by = 'site_id') %>% select(-site_id)
+#' function is nearly identical to `zip_prediction_groups`. We should modify and merge...but right now that build takes a long time
+#' and I am avoiding merging these two functions for that reason. 
+#' 
+#' How to modify and merge: 
+#' 1) could make `build_predict_df` more generic and allow us to specify the `out_file` suffix
+#' 2) could add an argument to a generic function `zip_file_groups` that tells us what to do (will need to export differently too)
+zip_ice_flags_groups <- function(outfile, file_info_df, site_groups){
+  
+  model_feathers <- inner_join(file_info_df, site_groups, by = 'site_id') %>% 
+    mutate(out_file = str_remove(
+      paste0(tools::file_path_sans_ext(out_file), '_ice_flag.csv'), '_temperatures')
+    ) %>% 
+    select(-site_id)
   
   
   cd <- getwd()
   on.exit(setwd(cd))
   
-  groups <- unique(model_feathers$group_id)
+  groups <- rev(sort(unique(model_feathers$group_id)))
   data_files <- c()
   for (group in groups){
     zipfile <- paste0('tmp/ice_flags_', group, '.zip')
-    these_files <- model_feathers %>% filter(group_id == !!group) %>% pull(model_file)
+    these_files <- model_feathers %>% filter(group_id == !!group)
     
     zippath <- file.path(getwd(), zipfile)
     
-    these_files <- purrr::map(these_files, function(x){
-      fileout <- file.path(tempdir(), str_remove(paste0(tools::file_path_sans_ext(x), '_ice_flag.csv'), '_temperatures'))
-      feather::read_feather(file.path(model_dir, x)) %>%
-        select(date = DateTime, ice) %>%
+    for (i in 1:nrow(these_files)){
+      fileout <- file.path(tempdir(), these_files$out_file[i])
+      feather::read_feather(these_files$source_filepath[i]) %>%
+        select(date = DateTime, ice) %>% # <- note this line also differs from the temperature export
         mutate(date = as.Date(lubridate::ceiling_date(date, 'days'))) %>%
         write_csv(path = fileout)
       fileout
-    }) %>% purrr::reduce(c)
+    }
     
     setwd(tempdir())
-    zip(zippath, files = basename(these_files))
+    
+    zip(zippath, files = these_files$out_file)
+    unlink(these_files$out_file)
     setwd(cd)
     data_files <- c(data_files, zipfile)
   }
